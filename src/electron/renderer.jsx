221b0a4873c326dc3828a2f63f71b68c518806b8
HIROKI-MATSUMURA <hiroki@codeups.jp>
2025-02-10 01:22:10 +0900

    ファイル差分0210

--- src/electron/renderer.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // React 18+ 用のインポート
-import App from './components/App';
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // React 18 の createRoot
-root.render(<App />);
